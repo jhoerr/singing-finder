8e412770985156ca1c9e1caf36cc002166169bab
John Hoerr <jhoerr@iu.edu>
2017-06-08 18:07:06 -0400

    Add Date.fs: functions for parsing the singing calendar DSL
    * Convert e.g "second sunday and saturday before in January" to specific
    dates for a given year.
    * Take a list of such singing descriptors and reduce them to a
    user-defined date range.

---
+++ src/SingingFinder.Core/Date.fs
@@ -0,0 +1,114 @@
+﻿namespace SingingFinder.Core
+
+open System
+
+// first Sunday
+// first Sunday and Saturday before
+// Saturday before first Sunday
+// Friday and Saturday before first Sunday
+// Jan 1
+// Jan 1 - 3
+
+type Singing = {Month:int; Day:string; Name:string }
+type Days = {Start: DateTime; End: DateTime}
+
+type SingingDays = 
+    | Sunday
+    | Saturday
+    | SaturdayAndSunday
+    | FridayAndSaturday
+
+module Date=
+
+    // Find the date of the first Sunday in a given month and year
+    // Ex 1: The first Sunday in January 2017 is 1/1/2017
+    // Ex 2: The first Sunday in January 2018 is 1/7/2017
+    let findFirstSundayInMonth year month = 
+        let rec firstSundayIn' (date:DateTime) = 
+            match date.DayOfWeek with
+            | DayOfWeek.Sunday  -> date
+            | _                 -> firstSundayIn' (date.AddDays(1.0))
+
+        firstSundayIn' (DateTime(year, month, 1))
+
+    // Find the date of the Nth (e.g. first, second, etc) Sunday in a given month and year.
+    // Ex 1: The first (n=1) Sunday in January 2017 is 1/1/2017
+    // Ex 2: The second (n=2) Sunday in January 2017 is 1/8/2017
+    // Account for the fact that months can sometimes have an e.g. "fifth" Sunday, and sometimes not.
+    let dateOfNthSunday year month weekend =
+        let firstSunday = findFirstSundayInMonth year month
+        let weekOffset = weekend - 1
+        let nthSunday' = firstSunday.AddDays(7.0 * (float)(weekOffset))
+        if nthSunday'.Month = month then Some(nthSunday') else None
+
+    // parse the singing description to determine which days of the week the singing will be held on
+    let whichSingingDays (str:String) = 
+        match str.ToLower() with
+        | x when x.StartsWith("friday and saturday before") -> SingingDays.FridayAndSaturday        
+        | x when x.StartsWith("saturday before")            -> SingingDays.Saturday        
+        | x when x.EndsWith("sunday")                       -> SingingDays.Sunday
+        | x when x.EndsWith("and saturday before")          -> SingingDays.SaturdayAndSunday
+        | _ -> failwith ("could not determine days from: " + str)
+    
+    // parse the singing description to determine which weekend of the month the singing will be held on
+    let whichWeekend (str:String) = 
+        match str.ToLower() with
+        | x when x.Contains("first")    -> 1   
+        | x when x.Contains("second")   -> 2   
+        | x when x.Contains("third")    -> 3   
+        | x when x.Contains("fourth")   -> 4   
+        | x when x.Contains("fifth")    -> 5   
+        | _ -> failwith ("could not determine weekend from: " + str)
+
+    // sometimes singings are on a specific date (January 1st, December 26th, etc.)
+    let parseFromSpecificDate year month day =
+        let date = (sprintf "%d/%d/%d" month day year) |> DateTime.Parse
+        Some({Start=date; End=date})
+    
+    // figure the start/end dates of a singing based on the days of the week it falls on.
+    let calculateRelativeDates sunday (str:String) =
+        match whichSingingDays str with
+        | SingingDays.Sunday            -> {Start=sunday; End=sunday}
+        | SingingDays.Saturday          -> {Start=sunday.AddDays(-1.0); End=sunday.AddDays(-1.0)}
+        | SingingDays.SaturdayAndSunday -> {Start=sunday.AddDays(-1.0); End=sunday}
+        | SingingDays.FridayAndSaturday -> {Start=sunday.AddDays(-2.0); End=sunday.AddDays(-1.0)}
+
+    // determine if this singing will be held based on its scheduled weekend, and if so on what specific days
+    let parseFromRelativeDates year month (str:String) = 
+        let nthSunday = whichWeekend str |> dateOfNthSunday year month
+        match nthSunday with
+        | Some(x) ->    Some(calculateRelativeDates x str)
+        | _ ->          None
+
+    // determine the singing days from its secription
+    let parseSingingDates year month (str:String) =
+        let mutable day = 0;
+        if Int32.TryParse(str, &day) 
+            then parseFromSpecificDate year month day
+            else parseFromRelativeDates year month str  
+
+    // determine if two date ranges overlap
+    let dateRangesOverlap range1 range2 =
+        match (range1, range2) with
+        | (None, _) -> false
+        | (_, None) -> false
+        | (Some(x), Some(y)) -> x.Start <= y.End && y.Start <= x.End
+
+    // take a list of singings and determine which, if any, fall within a supplied date range.
+    let singingsWithinDateRange (days:Days) (singings:Singing list) = 
+        
+        let singingsWithinDateRangeDuringYear year singings = 
+            singings 
+            |> List.map (fun s -> (s, (parseSingingDates year s.Month s.Day)))
+            |> List.filter (fun (s,d) -> dateRangesOverlap (Some(days)) d)
+
+        // get a list of singings in each of the starting/ending years chosen by the user.
+        // this accounts for scenarios where the dates span the new year (e.g. 12/1/2017-1/1/2018)
+        let startYearSingings = singings |> singingsWithinDateRangeDuringYear days.Start.Year
+        let endYearSingings = singings |> singingsWithinDateRangeDuringYear days.End.Year
+
+        // combine the results, sort them by date, and remove duplicates.
+        startYearSingings @ endYearSingings 
+        |> List.sortBy (fun (s,Some(d)) -> d.Start)
+        |> List.map (fun (s,d) -> s)
+        |> List.distinct
---
+++ tests/SingingFinder.Core.Tests/DateTests.fs
@@ -0,0 +1,200 @@
+﻿namespace SingingFinder.Core.Tests
+
+module DateTests=
+
+    open System
+    open SingingFinder.Core
+    open Swensen.Unquote
+    open NUnit.Framework
+
+    open Date
+
+    // firstSunday
+
+    [<Test>]
+    let ``first sunday: year is correct`` () =
+        test <@ (findFirstSundayInMonth 2017 1).Year = 2017 @>
+
+    [<Test>]
+    let ``first sunday: month is correct`` () =
+        test <@ (findFirstSundayInMonth 2017 2).Month = 2 @>
+
+    [<Test>]
+    let ``first sunday: day is correct (Jan 2017)`` () =
+        test <@ (findFirstSundayInMonth 2017 1).Day = 1 @>
+
+    [<Test>]
+    let ``first sunday: day is correct (Feb 2017)`` () =
+        test <@ (findFirstSundayInMonth 2017 2).Day = 5 @>
+
+    // nthSunday
+
+    [<Test>]
+    let ``first sunday in Jan 2017`` () =
+        test <@ dateOfNthSunday 2017 1 1 = Some(DateTime(2017, 1, 1)) @>
+
+    [<Test>]
+    let ``second sunday in Jan 2017`` () =
+        test <@ dateOfNthSunday 2017 1 2 = Some(DateTime(2017, 1, 8)) @>
+
+    [<Test>]
+    let ``fifth sunday in Jan 2017`` () =
+        test <@ dateOfNthSunday 2017 1 5 = Some(DateTime(2017, 1, 29)) @>
+
+    let ``fifth sunday in Jan 2018`` () =
+        test <@ dateOfNthSunday 2018 1 5 = None @>
+
+    // parseSingingDays
+
+    [<Test>]
+    let ``sunday`` () =
+        test <@ whichSingingDays "First Sunday" = SingingDays.Sunday @>
+
+    [<Test>]
+    let ``saturday`` () =
+        test <@ whichSingingDays "Saturday before First Sunday" = SingingDays.Saturday @>
+
+    [<Test>]
+    let ``friday and saturday`` () =
+        test <@ whichSingingDays "Friday and Saturday before First Sunday" = SingingDays.FridayAndSaturday @>
+
+    [<Test>]
+    let ``saturday and sunday`` () =
+        test <@ whichSingingDays "First Sunday and Saturday Before" = SingingDays.SaturdayAndSunday @>
+
+    // parseSinginWeekend
+    let parseSingingDatesTestData =
+        [
+            TestCaseData(2017, 2, "14").Returns(Some({Start=DateTime(2017,2,14); End=DateTime(2017,2,14)}))
+            TestCaseData(2018, 2, "14").Returns(Some({Start=DateTime(2018,2,14); End=DateTime(2018,2,14)}))
+            TestCaseData(2017, 12, "25").Returns(Some({Start=DateTime(2017,12,25); End=DateTime(2017,12,25)}))
+            TestCaseData(2018, 12, "25").Returns(Some({Start=DateTime(2018,12,25); End=DateTime(2018,12,25)}))
+            //
+            TestCaseData(2017, 1, "First Sunday").Returns(Some({Start=DateTime(2017,1,1); End=DateTime(2017,1,1)}))
+            TestCaseData(2018, 1, "First Sunday").Returns(Some({Start=DateTime(2018,1,7); End=DateTime(2018,1,7)})) 
+            TestCaseData(2017, 2, "First Sunday").Returns(Some({Start=DateTime(2017,2,5); End=DateTime(2017,2,5)}))
+            TestCaseData(2018, 2, "First Sunday").Returns(Some({Start=DateTime(2018,2,4); End=DateTime(2018,2,4)}))
+            //
+            TestCaseData(2017, 1, "Second Sunday").Returns(Some({Start=DateTime(2017,1,8); End=DateTime(2017,1,8)}))
+            TestCaseData(2018, 1, "Second Sunday").Returns(Some({Start=DateTime(2018,1,14); End=DateTime(2018,1,14)}))
+            TestCaseData(2017, 2, "Second Sunday").Returns(Some({Start=DateTime(2017,2,12); End=DateTime(2017,2,12)}))
+            TestCaseData(2018, 2, "Second Sunday").Returns(Some({Start=DateTime(2018,2,11); End=DateTime(2018,2,11)}))
+            //
+            TestCaseData(2017, 1, "Fifth Sunday").Returns(Some({Start=DateTime(2017,1,29); End=DateTime(2017,1,29)}))
+            TestCaseData(2018, 1, "Fifth Sunday").Returns(None)
+            //
+            TestCaseData(2017, 1, "Saturday before First Sunday").Returns(Some({Start=DateTime(2016,12,31); End=DateTime(2016,12,31)}))
+            TestCaseData(2018, 1, "Saturday before First Sunday").Returns(Some({Start=DateTime(2018,1,6); End=DateTime(2018,1,6)}))
+            TestCaseData(2017, 2, "Saturday before First Sunday").Returns(Some({Start=DateTime(2017,2,4); End=DateTime(2017,2,4)}))
+            TestCaseData(2018, 2, "Saturday before First Sunday").Returns(Some({Start=DateTime(2018,2,3); End=DateTime(2018,2,3)}))
+            //
+            TestCaseData(2017, 1, "Saturday before Second Sunday").Returns(Some({Start=DateTime(2017,1,7); End=DateTime(2017,1,7)}))
+            TestCaseData(2018, 1, "Saturday before Second Sunday").Returns(Some({Start=DateTime(2018,1,13); End=DateTime(2018,1,13)}))
+            TestCaseData(2017, 2, "Saturday before Second Sunday").Returns(Some({Start=DateTime(2017,2,11); End=DateTime(2017,2,11)}))
+            TestCaseData(2018, 2, "Saturday before Second Sunday").Returns(Some({Start=DateTime(2018,2,10); End=DateTime(2018,2,10)}))
+            //
+            TestCaseData(2017, 1, "Saturday before Fifth Sunday").Returns(Some({Start=DateTime(2017,1,28); End=DateTime(2017,1,28)}))
+            TestCaseData(2018, 1, "Saturday before Fifth Sunday").Returns(None)
+            //
+            TestCaseData(2017, 1, "First Sunday and Saturday Before").Returns(Some({Start=DateTime(2016,12,31); End=DateTime(2017,1,1)}))
+            TestCaseData(2018, 1, "First Sunday and Saturday Before").Returns(Some({Start=DateTime(2018,1,6); End=DateTime(2018,1,7)})) 
+            TestCaseData(2017, 2, "First Sunday and Saturday Before").Returns(Some({Start=DateTime(2017,2,4); End=DateTime(2017,2,5)}))
+            TestCaseData(2018, 2, "First Sunday and Saturday Before").Returns(Some({Start=DateTime(2018,2,3); End=DateTime(2018,2,4)}))
+            //
+            TestCaseData(2017, 1, "Second Sunday and Saturday Before").Returns(Some({Start=DateTime(2017,1,7); End=DateTime(2017,1,8)}))
+            TestCaseData(2018, 1, "Second Sunday and Saturday Before").Returns(Some({Start=DateTime(2018,1,13); End=DateTime(2018,1,14)}))
+            TestCaseData(2017, 2, "Second Sunday and Saturday Before").Returns(Some({Start=DateTime(2017,2,11); End=DateTime(2017,2,12)}))
+            TestCaseData(2018, 2, "Second Sunday and Saturday Before").Returns(Some({Start=DateTime(2018,2,10); End=DateTime(2018,2,11)}))
+            //
+            TestCaseData(2017, 1, "Fifth Sunday and Saturday Before").Returns(Some({Start=DateTime(2017,1,28); End=DateTime(2017,1,29)}))
+            TestCaseData(2018, 1, "Fifth Sunday and Saturday Before").Returns(None)
+            //
+            TestCaseData(2017, 1, "Friday and Saturday before First Sunday").Returns(Some({Start=DateTime(2016,12,30); End=DateTime(2016,12,31)}))
+            TestCaseData(2018, 1, "Friday and Saturday before First Sunday").Returns(Some({Start=DateTime(2018,1,5); End=DateTime(2018,1,6)}))
+            TestCaseData(2017, 2, "Friday and Saturday before First Sunday").Returns(Some({Start=DateTime(2017,2,3); End=DateTime(2017,2,4)}))
+            TestCaseData(2018, 2, "Friday and Saturday before First Sunday").Returns(Some({Start=DateTime(2018,2,2); End=DateTime(2018,2,3)}))
+            //
+            TestCaseData(2017, 1, "Friday and Saturday before Second Sunday").Returns(Some({Start=DateTime(2017,1,6); End=DateTime(2017,1,7)}))
+            TestCaseData(2018, 1, "Friday and Saturday before Second Sunday").Returns(Some({Start=DateTime(2018,1,12); End=DateTime(2018,1,13)}))
+            TestCaseData(2017, 2, "Friday and Saturday before Second Sunday").Returns(Some({Start=DateTime(2017,2,10); End=DateTime(2017,2,11)}))
+            TestCaseData(2018, 2, "Friday and Saturday before Second Sunday").Returns(Some({Start=DateTime(2018,2,9); End=DateTime(2018,2,10)}))
+            //
+            TestCaseData(2017, 1, "Friday and Saturday before Fifth Sunday").Returns(Some({Start=DateTime(2017,1,27); End=DateTime(2017,1,28)}))
+            TestCaseData(2018, 1, "Friday and Saturday before Fifth Sunday").Returns(None)
+        ]
+    
+    [<TestCaseSource("parseSingingDatesTestData")>]
+    let ``parse singing dates`` year month (str:String) = 
+        parseSingingDates year month str
+
+    // dateWithinRange
+
+    let dateWithinRangeTestData =
+        [
+            // these represent user-selected start/end dates for a search.
+            // the test date range is 1/1/2017-1/3/2017
+            TestCaseData("1/1/2017", "1/1/2017").Returns(true)
+            TestCaseData("1/2/2017", "1/2/2017").Returns(true)
+            TestCaseData("1/3/2017", "1/3/2017").Returns(true)
+            TestCaseData("12/31/2016", "1/1/2017").Returns(true)
+            TestCaseData("1/3/2017", "1/4/2017").Returns(true)
+            TestCaseData("12/31/2016", "1/4/2017").Returns(true)
+            // negative e
+            TestCaseData("12/31/2016", "12/31/2016").Returns(false)
+            TestCaseData("1/4/2017", "1/4/2017").Returns(false)
+            // far past
+            TestCaseData("1/1/2016", "1/1/2016").Returns(false)
+            TestCaseData("1/2/2016", "1/2/2016").Returns(false)
+            TestCaseData("1/3/2016", "1/3/2016").Returns(false)
+            TestCaseData("12/31/2015", "1/1/2016").Returns(false)
+            TestCaseData("1/3/2016", "1/4/2016").Returns(false)
+            TestCaseData("12/31/2015", "1/4/2016").Returns(false)
+            // far future
+            TestCaseData("1/1/2018", "1/1/2018").Returns(false)
+            TestCaseData("1/2/2018", "1/2/2018").Returns(false)
+            TestCaseData("1/3/2018", "1/3/2018").Returns(false)
+            TestCaseData("12/31/2017","1/1/2018").Returns(false)
+            TestCaseData("1/3/2018", "1/4/2018").Returns(false)
+            TestCaseData("12/31/2017", "1/4/2018").Returns(false)
+        ]
+
+    [<TestCaseSource("dateWithinRangeTestData")>]
+    let ``date in range`` (candidateStart:String) (candidateEnd:String) = 
+        let range1 = {Start=DateTime(2017,1,1); End=DateTime(2017,1,3)}
+        let range2 = {Start=DateTime.Parse(candidateStart); End=DateTime.Parse(candidateEnd)}
+        dateRangesOverlap (Some(range1)) (Some(range2))
+
+    [<Test>]
+    let ``date in range: none case (left)`` () =
+        test <@ dateRangesOverlap (Some({Start=DateTime(2017,1,1); End=DateTime(2017,1,3)})) None = false @>
+
+    [<Test>]
+    let ``date in range: none case (right)`` () =
+        test <@ dateRangesOverlap (Some({Start=DateTime(2017,1,1); End=DateTime(2017,1,3)})) None = false @>
+
+    // singingsWithinDateRange
+
+    let newYearsDay =   {Month=1;   Day="1";            Name="new years day"}
+    let christmasDay =  {Month=12;  Day="25";           Name="christmas day"}
+    let firstSunInJan = {Month=1;   Day="First Sunday"; Name="first sunday in january"}
+
+    let singings = 
+        [
+            newYearsDay; 
+            christmasDay; 
+            firstSunInJan
+        ]
+
+    let singingsWithinDateRangeTestData =
+        [
+            TestCaseData("1/1/2017", "1/1/2017").Returns([newYearsDay; firstSunInJan])
+            TestCaseData("1/1/2018", "1/1/2018").Returns([newYearsDay])
+            TestCaseData("1/1/2018", "1/15/2018").Returns([newYearsDay; firstSunInJan])
+            TestCaseData("12/20/2017", "12/31/2017").Returns([christmasDay])
+            TestCaseData("12/1/2017", "1/1/2018").Returns([christmasDay; newYearsDay])
+            TestCaseData("1/1/2017", "1/1/2018").Returns([newYearsDay; firstSunInJan; christmasDay])
+        ]
+
+    [<TestCaseSource("singingsWithinDateRangeTestData")>]
+    let ``singings within date range`` (startDay:String) (endDay:String) =
+        singings |> singingsWithinDateRange {Start=DateTime.Parse(startDay); End=DateTime.Parse(endDay)}
