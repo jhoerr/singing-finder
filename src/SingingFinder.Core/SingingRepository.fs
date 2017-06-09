67ffa67e0a1ea36dc4e4cef7ad395d71a8c21347
John Hoerr <jhoerr@iu.edu>
2017-06-09 18:12:11 -0400

    Add SingingRepository to provide canonical singing data

---
+++ src/SingingFinder.Core/SingingRepository.fs
@@ -0,0 +1,23 @@
+﻿namespace SingingFinder.Core
+
+open System
+
+module SingingRepository =
+
+    // to do: replace this with sql query
+    let singings = 
+        [
+            {Name = "Ohio State Convention"; Day = "First Saturday and Sunday Before"; Month = 3}
+            {Name = "Missouri State Convention"; Day = "Second Saturday and Sunday Before"; Month = 3}
+            {Name = "Bloomington All-Day"; Day = "Saturday Before the Fourth Sunday"; Month = 10}
+            {Name = "Higher Ground"; Day = "Saturday Before the Fourth Sunday"; Month = 4}
+        ]
+
+    // to do: replace this with sql query
+    let singingsInRange (lattitude:float,longitude:float) rangeInMiles = 
+        [
+            {Name = "Bloomington All-Day"; Day = "Saturday Before the Fourth Sunday"; Month = 10}
+            {Name = "Higher Ground"; Day = "Saturday Before the Fourth Sunday"; Month = 4}
+        ]
+
+
