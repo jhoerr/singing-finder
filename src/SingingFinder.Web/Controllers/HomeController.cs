fa3c8d50364fdfad0d6a49f5a38c6873092219be
John Hoerr <jhoerr@gmail.com>
2017-06-11 21:35:27 -0400

    Switch to Net framework web app
    This will enable use of F# + Type Providers

--- src/SingingFinder.Web/Controllers/HomeController.cs
+++ src/SingingFinder.Web/Controllers/HomeController.cs
@@ -1,37 +1,18 @@
-﻿using SingingFinder.Web.Models;
-using Microsoft.FSharp.Core;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace SingingFinder.Web.Controllers
 {
-    /// <summary>
-    /// A controller intercepts the incoming browser request and returns
-    /// an HTML view (.cshtml file) or any other type of data.
-    /// </summary>
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            // The view being returned is calculated based on the name of the
-            // controller (Home) and the name of the action method (Index).
-            // So in this case, the view returned is /Views/Home/Index.cshtml.
+            ViewBag.Title = "Home Page";
+
             return View();
-        }
-
-        public IActionResult About()
-        {
-            // Creates a model and passes it on to the view.
-            Employee[] model =
-            {
-                new Employee { Name = "Alfred", Title = "Manager" },
-                new Employee { Name = "Sarah", Title = "Accountant" }
-            };
-
-            SingingFinder.Core.Date.findFirstSundayInMonth
-
-
-            return View(model);
         }
     }
 }
---
+++ src/SingingFinder.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SingingFinder.Web.WebApiApplication" Language="C#" %>
