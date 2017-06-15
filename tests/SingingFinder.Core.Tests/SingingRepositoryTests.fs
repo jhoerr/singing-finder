ead09afcf2063c47bdb527052e5897b661f1e010
John Hoerr <jhoerr@gmail.com>
2017-06-15 09:51:17 -0400

    Add integration tests for SingingRepository

---
+++ tests/SingingFinder.Core.Tests/SingingRepositoryTests.fs
@@ -0,0 +1,24 @@
+﻿namespace SingingFinder.Core.Tests
+
+module SingingRepositoryTests=
+
+    open System
+    open SingingFinder.Core
+    open Swensen.Unquote
+    open NUnit.Framework
+    open SingingRepository
+
+    [<Test>]
+    [<Category "integration">]
+    let ``time fetch all singings``()=
+        let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+        let singings' = singings()
+        stopWatch.Stop()
+        printfn "%f ms\n\n" stopWatch.Elapsed.TotalMilliseconds
+    
+    [<Test>]
+    [<Category "integration">]
+    let ``fetch all singings``()=
+        singings()
+        |> List.iter (fun s -> printf "%s\n" (s.Name))
+
