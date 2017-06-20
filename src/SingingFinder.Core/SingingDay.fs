cd4e78e2d90a60def8d6272c69d1bd69ab2f06be
John Hoerr <jhoerr@gmail.com>
2017-06-20 13:28:41 -0400

    Implement parser for singing day descriptions
    Handles typical scenarios like:
    + every monday
    + second and 4th tuesdays
    + second sunday
    + saturday before second sunday
    + second sunday and saturday before
    
    Also handle exotic scenarios like:
    + 1st, 3rd, and last thursdays
    + friday and saturday before 2nd and 4th sunday

---
+++ src/SingingFinder.Core/SingingDay.fs
@@ -0,0 +1,20 @@
+﻿namespace SingingFinder.Core 
+
+open System
+
+type Cardinality = First | Second | Third | Fourth | Fifth | Last
+
+type SingingDay =
+    // 25                           -> 25
+    | DayOfMonth of int                    
+    // Every Monday                 -> Monday
+    | Every of DayOfWeek
+    // 1st Monday                   -> [First] * Monday
+    // 2nd and 4th Tuesday          -> [Second, Fourth] * Tuesday
+    | Regularly of Cardinality list * DayOfWeek
+    // Fri and Sat before 2nd Sun   -> Second * Sunday * [Friday, Saturday]
+    // 2nd Sun and Sat Before       -> Second * Sunday * [Saturday, Sunday]
+    // Sat before 2nd Tuesday       -> Second * Tuesday * [Saturday]
+    | OnOrBefore of Cardinality list * DayOfWeek * DayOfWeek list
+                                            
+                                            
---
+++ tests/SingingFinder.Core.Tests/ParserTests.fs
@@ -0,0 +1,79 @@
+﻿namespace SingingFinder.Core.Tests
+
+
+module ParserTests=
+
+    open System
+    open System.IO
+    open Microsoft.FSharp.Text.Lexing
+    open SingingFinder.Core
+    open Parser
+
+    open FsUnit
+    open NUnit.Framework
+    open Swensen.Unquote
+
+    let parse (singing:string) =
+        let lexbuf = singing.ToLowerInvariant() |> LexBuffer<char>.FromString
+        Parser.start Lexer.read lexbuf
+    
+    [<Test>]
+    let ``specific day``()=
+        test <@ "1" |> parse = DayOfMonth(1) @>
+
+    [<Test>]
+    let ``every sunday``()=
+        test <@ "every monday" |> parse = Every(DayOfWeek.Monday) @>
+
+    [<Test>]
+    let ``first sunday``()=
+        test <@ "first sunday" |> parse = Regularly([First], DayOfWeek.Sunday) @>
+
+    [<Test>]
+    let ``1st sunday``()=
+        test <@ "1st sunday" |> parse = Regularly([First], DayOfWeek.Sunday) @>
+
+    [<Test>]
+    let ``first and third monday``()=
+        test <@ "first and third monday" |> parse = Regularly([First; Third], DayOfWeek.Monday) @>
+
+    [<Test>]
+    let ``fiRST AnD THIRD monday``()=
+        test <@ "fiRST AnD THIRD monday" |> parse = Regularly([First; Third], DayOfWeek.Monday) @>
+
+    [<Test>]
+    let ``first and third mondays``()=
+        test <@ "first and third mondays" |> parse = Regularly([First; Third], DayOfWeek.Monday) @>
+
+    [<Test>]
+    let ``first sunday and saturday before``()=
+        test <@ "first sunday and saturday before" |> parse = OnOrBefore([First], DayOfWeek.Sunday, [DayOfWeek.Saturday; DayOfWeek.Sunday]) @>
+
+    [<Test>]
+    let ``saturday before first sunday``()=
+        test <@ "saturday before first sunday" |> parse = OnOrBefore([First], DayOfWeek.Sunday, [DayOfWeek.Saturday]) @>
+
+    [<Test>]
+    let ``saturday before first and third sunday``()=
+        test <@ "saturday before first and third sunday" |> parse = OnOrBefore([First; Third], DayOfWeek.Sunday, [DayOfWeek.Saturday]) @>
+
+    [<Test>]
+    let ``saturday before first, third, and fifth sunday (oxford comma)``()=
+        test <@ "saturday before first, third, and fifth sunday" |> parse = OnOrBefore([First; Third; Fifth], DayOfWeek.Sunday, [DayOfWeek.Saturday]) @>
+
+    [<Test>]
+    let ``saturday before first, third and fifth sunday (no oxford comma)``()=
+        test <@ "saturday before first, third and fifth sunday" |> parse = OnOrBefore([First; Third; Fifth], DayOfWeek.Sunday, [DayOfWeek.Saturday]) @>
+
+    [<Test>]
+    let ``thursday and friday and saturday before first and third sunday``()=
+        test <@ "thursday and friday and saturday before first and third sunday" |> parse = OnOrBefore([First; Third], DayOfWeek.Sunday, [DayOfWeek.Thursday; DayOfWeek.Friday; DayOfWeek.Saturday]) @>
+
+    [<Test>]
+    let ``thursday, friday, and saturday before first and third sunday``()=
+        test <@ "thursday, friday, and saturday before first and third sunday" |> parse = OnOrBefore([First; Third], DayOfWeek.Sunday, [DayOfWeek.Thursday; DayOfWeek.Friday; DayOfWeek.Saturday]) @>
+
+    [<Test>]
+    let ``thursday, friday, saturday before first, third and fifth sunday``()=
+        test <@ "thursday, friday, saturday before first, third and fifth sunday" |> parse = OnOrBefore([First; Third; Fifth], DayOfWeek.Sunday, [DayOfWeek.Thursday; DayOfWeek.Friday; DayOfWeek.Saturday]) @>
+    
